23bc741f11d8c5822dc0ced6187807e82ba2023d
desiree <deemenezes@hotmail.com>
2018-08-22 15:40:38 -0300

    calculo media

--- media.py
+++ media.py
@@ -1,5 +1,15 @@
-n1 = (input('digite a primeira nota:'))
-n2 = (input('digite a segunda nota:'))
 
-media = ((n1 + n2) / 2)
-print('A média calculada é: ', float(media))
+print("**** CALCULO MEDIA PYTHON SYNTAX ****")
+
+n1 = raw_input("Digite a primeira nota: ")
+n2 = raw_input("Digite a segunda nota: ")
+
+media = (float(n1) + float(n2))/2
+
+if media > 6:
+    print("PARABENS, VOCE ESTA APROVADO")
+else:
+    print("VOCE ESTA REPROVADO")
+
+print("MEDIA FINAL: %s" % media)
+
