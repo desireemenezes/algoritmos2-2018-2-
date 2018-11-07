da96f9f5616996936b393dd27c8c928cab5aeedc
deemenezes@hotmail.com <deemenezes@hotmail.com>
2018-11-06 23:46:16 -0200

    adc lista encadeada

---
+++ execicios/listasEncadeada/no.py
@@ -0,0 +1,12 @@
+class No:
+    def __init__(self, dado):
+        self.dado = dado
+        self.proximo = None #valor simbolico vazio para o proximo que nao foi declarado como atributo
+
+#no = No(5)  
+#no1 = No(6)
+#resultado1 = no.dado 
+#resultado2 = no.proximo = no1
+#
+#print(resultado1)
+#print(resultado2)
