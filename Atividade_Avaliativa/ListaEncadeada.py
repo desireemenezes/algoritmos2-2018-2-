996e2bbe2047e49e9b0b693f2854b175e15b4e24
deemenezes@hotmail.com <deemenezes@hotmail.com>
2018-12-12 08:45:34 -0200

    commit fix

--- Atividade_Avaliativa/ListaEncadeada.py
+++ Atividade_Avaliativa/ListaEncadeada.py
@@ -26,9 +26,7 @@ class ListaEncadeada:
             self._cabeca = novo
         self.size += 1 #incrementa o valor que já existia no size
 
-    
-    
-    
+  
     def addFinal(self, e):
         """Insere elemento no FINAL da lista"""
         if self._rabo is None:
@@ -37,6 +35,24 @@ class ListaEncadeada:
             self._rabo.proximo = No(e)
             self._rabo = self._rabo.proximo
         self.size += 1 # incrementa o valor que já existia no size
+
+
+    def buscaIndice(self, e):
+        aux = self._cabeca
+        cont = 0
+        while aux is not None:
+            if (cont % 4) == 0:
+                print(aux)
+            cont += 1
+            aux = aux.proximo
+    
+    def __str__(self):
+        aux = self._cabeca
+        texto = "Lista: \n"
+        while aux is not None:
+            texto += str(aux)+"\n"
+            aux = aux.proximo
+        return texto
 
 
 
@@ -62,9 +78,12 @@ lista.addFinal(8)
 lista.addInicio(10)
 lista.addFinal(9)
 lista.addFinal(11)
+lista.addFinal(112)
+lista.addFinal(13)
 lista.addInicio(55)
+lista.buscaIndice(3)
 print("lista {}".format(lista))
 print("tamanho da lista {}".format(lista.size))
 print("primeiro elemento {}".format(lista.primeiro.dado))
 print("umtimo elemento {}".format(lista.ultimo.dado))
-#print("remove primeiro elemento da lista {}".format(lista.ultimo.dado))
+print("busca por indice {}".format(lista))
